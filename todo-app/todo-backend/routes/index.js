79d00e83cd9593ad64890e133b30a726313c1153
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2025-01-31 18:41:30 +0200

    Exercises 12.9. - 12.11.

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
@@ -11,8 +12,16 @@ router.get('/', async (req, res) => {
 
   res.send({
     ...configs,
-    visits
-  });
-});
+    visits,
+  })
+})
 
-module.exports = router;
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await redis.getAsync('added_todos')
+
+  res.send({
+    added_todos: addedTodos || 0,
+  })
+})
+
+module.exports = router
